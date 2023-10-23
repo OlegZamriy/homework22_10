a65a5dc0a4fcf788391757d1ecc98a0cff16e453
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:40:27 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
- 
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Enter a six-digit number: ";
+    string input;
+    cin >> input;
+
+    if (input.length() != 6) {
+        cout << "Error: The entered number is not a six-digit number." << endl;
+        return 1;  // Exit the program with an error code
+    }
+
+    int sum1 = (input[0] - '0') + (input[1] - '0') + (input[2] - '0');
+    int sum2 = (input[3] - '0') + (input[4] - '0') + (input[5] - '0');
+
+    if (sum1 == sum2) {
+        cout << "This is a happy number!" << endl;
+    }
+    else {
+        cout << "This is not a happy number." << endl;
+    }
+
+    return 0;
+}
