567564cb88c3345196c058cb9528f0369da464e9
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:39:07 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
- 
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    int firstNumber, secondNumber;
+
+    cout << "¬вед≥ть перше число: ";
+    cin >> firstNumber;
+
+    cout << "¬вед≥ть друге число: ";
+    cin >> secondNumber;
+
+    if (firstNumber == secondNumber) {
+        cout << "„исла р≥вн≥: " << firstNumber << " ≥ " << secondNumber << endl;
+    }
+    else {
+        int minNumber = (firstNumber < secondNumber) ? firstNumber : secondNumber;
+        int maxNumber = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+
+        cout << "„исла у пор€дку зростанн€: " << minNumber << " ≥ " << maxNumber << endl;
+    }
+
+    return 0;
+}
