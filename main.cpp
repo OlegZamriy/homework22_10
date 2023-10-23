183cddf0015e80f25b68cfe73241520b1435ae5f
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:34:37 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
- 
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << "number " << number << " is even." << endl;
+    }
+    else {
+        cout << "number " << number << " is odd." << endl;
+    }
+
+    return 0;
+}
