d211bdaf8d7279661aa5346d18bc3612115be466
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:43:08 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
- 
+using namespace std;
+
+int main() {
+    int maxNumber, currentNumber;
+
+    cout << "Enter 7 integers, one at a time:" << endl;
+
+    for (int i = 0; i < 7; i++) {
+        cout << "Enter number " << (i + 1) << ": ";
+        cin >> currentNumber;
+
+        if (i == 0 || currentNumber > maxNumber) {
+            maxNumber = currentNumber;
+        }
+    }
+
+    cout << "The maximum number is: " << maxNumber << endl;
+
+    return 0;
+}
