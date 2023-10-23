2bd5a38906e7a8b39a49eba4c58f7b891bcb643a
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:35:48 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
- 
+using namespace std;
+
+int main() {
+    int firstNumber, secondNumber;
+
+    cout << "Enter the first number: ";
+    cin >> firstNumber;
+
+    cout << "Enter the second number: ";
+    cin >> secondNumber;
+
+    if (firstNumber < secondNumber) {
+        cout << "The smaller number is: " << firstNumber << endl;
+    }
+    else if (secondNumber < firstNumber) {
+        cout << "The smaller number is: " << secondNumber << endl;
+    }
+    else {
+        cout << "The numbers are equal: " << firstNumber << endl;
+    }
+
+    return 0;
+}
