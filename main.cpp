11208944f66e2a2db458f05e98fd19a17bcbbf6d
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:36:41 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
- 
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number > 0) {
+        cout << "This is a positive number." << endl;
+    }
+    else if (number < 0) {
+        cout << "This is a negative number." << endl;
+    }
+    else {
+        cout << "This number is equal to zero." << endl;
+    }
+
+    return 0;
+}
